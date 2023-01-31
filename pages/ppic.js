1b19a2b59a4c4ad2a4bab1bb09c5a72737fe2dfa
aji <wiji@atop.com.tw>
2023-01-31 23:10:03 +0700

    update

--- pages/ppic.js
+++ pages/ppic.js
@@ -3,15 +3,15 @@ import PPICForm from "@/components/forms/PPIC";
 export default function Page() {
   return (
     <>
-      <header className='text-slate-700'>
-        <div className='flex p-2 md:p-4'>
-          <button className='hover:text-slate-500'>sign out</button>
+      <header className='text-slate-700 text-sm md:text-base'>
+        <div className='flex flex-row-reverse py-4 px-6'>
+          <button className='hover:text-slate-500 active:text-slate-400'>sign out</button>
         </div>
       </header>
 
       <main>
-        <div className='py-8 px-8 md:px-4 md:py-12 md:w-[768px] md:mx-auto'>
-          <h1 className='text-2xl md:text-4xl mb-4 md:mb-8'>PPIC Form</h1>
+        <div className='px-4 py-10 md:py-16 md:w-[768px] md:mx-auto'>
+          <h1 className='text-2xl md:text-4xl mb-8 md:mb-12'>PPIC Form</h1>
 
           <PPICForm />
         </div>
